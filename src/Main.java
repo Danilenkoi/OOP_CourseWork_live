15ffda25fe3019cda466b659e73ac0a7eee7b7fb
iladanilenko <Danilenkoi@inbox.ru>
2023-03-06 14:57:07 +0300

    Задача на live

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        HashMap<String,String> greetingWish = new HashMap<String,String>();
+
+        greetingWish.put("Hello!", " have a nice day!");
+        greetingWish.put("Привет!", " хорошего тебе дня!");
+
+        System.out.println(greetingWish.size());
+
+        // Пример цикла While
+
+        Iterator iter = greetingWish.entrySet().iterator();
+        while(iter.hasNext()) {
+            Map.Entry sayHello = (Map.Entry) iter.next();
+            System.out.println("Приветствие: " + sayHello.getKey() + "; Пожелание хорошего дня: " + sayHello.getValue());
+        }
+
+        // Пример цикла For
+
+        for(Map.Entry sayHello1: greetingWish.entrySet()) {
+            System.out.println("Приветствие: " + sayHello1.getKey() + "; Пожелание хорошего дня: " + sayHello1.getValue());
+
+        }
     }
 }
